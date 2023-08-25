22060bffee3de98d0cab9490676e84fbe4bd8292
Shilu <shilu842003@gmail.com>
2023-08-25 06:40:25 +0530

    added conv_layer

--- mobilevit/layers/conv_layers.py
+++ mobilevit/layers/conv_layers.py
@@ -119,3 +119,85 @@ class Conv_3x3_bn(tf.keras.layers.Layer):
     config["out_channels"] = self.out_channels
 
     return config
+
+
+## new 
+class MobileViTConvLayer(tf.keras.layers.Layer):
+  def __init__(self, 
+               out_channels: int, 
+               kernel_size: int | Tuple, 
+               config: ConfigDict,
+               strides: int | Tuple = 1, 
+               groups: int = 1, 
+               bias: bool = False, 
+               dilation: int = 1, 
+               use_norm: bool = True, 
+               use_act: Union[bool, str] = True, 
+               **kwargs)-> None:
+    super(MobileViTConvLayer, self).__init__(**kwargs)
+    self.config = config 
+      
+    padding = int((kernel_size - 1) / 2) * dilation
+    self.padding = tf.keras.layers.ZeroPadding2D(padding)
+
+    if out_channels % groups != 0:
+      raise ValueError(f"Output channels ({out_channels}) are not divisible by {groups} groups.")
+
+    self.conv = tf.keras.layers.Conv2D(
+                    kernel_size = kernel_size,
+                    filters = out_channels,
+                    strides = strides,
+                    groups = groups,
+                    use_bias = bias,
+                    dilation_rate = dilation
+              )
+    
+    self.norm = None
+    if use_norm: 
+      self.norm = tf.keras.layers.BatchNormalization(epsilon=1e-5, 
+                                                     momentum=0.1, 
+                                                     name="normalization")
+
+    self.activation = None
+    if use_act:
+      if isinstance(use_act, str):
+        _act = act_layer_factory(use_act)
+        self.activation = _act()
+      elif isinstance(config.hidden_act, str):
+        _act = act_layer_factory(config.hidden_act)
+        self.activation = _act()
+      else:
+        self.activation = config.hidden_act 
+
+    self.use_norm = use_norm
+    self.use_act = use_act 
+
+  def call(self, 
+           x: tf.Tensor, 
+           training: bool = False) -> tf.Tensor:
+
+    padding_x = self.padding(x)
+    x = self.conv(padding_x)
+
+    if self.use_norm:
+      x = self.norm(x)
+
+    if self.use_act:
+      x = self.activation(x)
+    
+    return x 
+
+  def get_config(self)->Dict:
+    config = super().get_config()
+    config.update(
+        {
+            "use_norm": self.use_norm,
+            'use_act': self.use_act,
+        }
+    )
+
+    return config
+
+  @classmethod
+  def from_config(cls, config):
+    return cls(**config)
