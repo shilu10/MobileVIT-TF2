77eb7afb969f4d12019888050cca67d449eae1a0
adminuser <adminuser@kali.mvm>
2023-08-25 06:53:13 +0530

    added new inverted-residual block

--- mobilevit/blocks/invertedresidual.py
+++ mobilevit/blocks/invertedresidual.py
@@ -96,3 +96,81 @@ class InvertedResidual(tf.keras.layers.Layer):
         config['expansion'] = self.expansion
 
         return config
+
+
+## new 
+class InvertedResidualLayer(tf.keras.layers.Layer):
+  def __init__(self, 
+               config: ConfigDict, 
+               in_channels: int, 
+               out_channels: int, 
+               stride: int, 
+               dilation: int = 1, 
+               **kwargs) -> None:
+    super(InvertedResidualLayer, self).__init__(**kwargs)
+
+    expanded_channels = make_divisible(int(round(in_channels * config.expand_ratio)), 8)
+
+    if stride not in [1, 2]:
+      raise ValueError(f"Invalid stride {stride}.")
+
+    self.use_residual = (stride == 1) and (in_channels == out_channels)
+
+    # pw
+    # down-sample in the first conv
+    self.expand_1x1 = MobileViTConvLayer(
+            config, out_channels=expanded_channels, kernel_size=1, name="expand_1x1"
+        )
+    
+    # DW
+    self.conv_3x3 = MobileViTConvLayer(
+            config,
+            out_channels=expanded_channels,
+            kernel_size=3,
+            stride=stride,
+            groups=expanded_channels,
+            dilation=dilation,
+            name="conv_3x3",
+        )
+    
+    # pw
+    # up-smaple in last conv
+    self.reduce_1x1 = MobileViTConvLayer(
+            config,
+            out_channels=out_channels,
+            kernel_size=1,
+            use_activation=False,
+            name="reduce_1x1",
+        )
+    
+    self.in_channels = in_channels 
+    self.out_channels = out_channels
+    
+  def call(self, 
+           x: tf.Tensor, 
+            training: bool = False) -> tf.Tensor:
+
+    # shortcut for the resiudal
+    shortcut = x
+
+    x = self.expand_1x1(x, training=training)
+    x = self.conv_3x3(x, training=training)
+    x = self.reduce_1x1(x, training=training)
+
+    return shortcut + x if self.use_residual else x
+  
+  def get_config(self) -> Dict:
+    config = super().get_config()
+    config.update(
+        {
+            "in_channels": self.in_channels,
+            "out_channels": self.out_channels
+        }
+      )
+    
+    return config 
+
+  @classmethod
+  def from_config(cls, config):
+    return cls(**config)
+ 
