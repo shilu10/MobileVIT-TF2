48b0da5e15ebb1f471d83380adaf320f23a41ebd
Shilu <shilu842003@gmail.com>
2023-08-28 19:10:21 +0530

    added the base_config and configs for all_model_variants

--- mobilevit/layers/__init__.py
+++ mobilevit/layers/__init__.py
@@ -1,7 +1,6 @@
 from .conv_layers import MobileViTConvLayer
 from .droppath import DropPath
 from .layerscale import LayerScale
-from .mha import MultiHeadSelfAttention
 from .factory import act_layer_factory, norm_layer_factory
 from .inverted_residual import InvertedResidualLayer
 from .mlp import MLP
